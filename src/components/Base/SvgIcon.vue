3a98002fac2dcb132486f489b31c4360d3ab3780
Julyme3 <jg0311@bk.ru>
2023-03-07 14:04:06 +0500

    add svg-loader

---
+++ src/components/Base/SvgIcon.vue
@@ -0,0 +1,22 @@
+<template>
+  <component class="svg-container" :is="icon" />
+</template>
+
+<script setup lang="ts">
+import {defineAsyncComponent, defineProps} from "vue";
+
+interface Props {
+  name: string
+}
+
+const props = defineProps<Props>()
+
+const icon = defineAsyncComponent(() => import(`../../assets/${props.name}.svg`));
+</script>
+
+<style scoped lang="less">
+.svg-container {
+  display: inline-flex;
+  cursor: pointer;
+}
+</style>
